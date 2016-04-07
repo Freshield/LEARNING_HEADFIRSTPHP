e6e69dd0de976ac43ae1e483e1a81f6ff3ee26e1
Freshield <zxdsw199182@gmail.com>
2016-04-06 22:46:28 -0400

    test new part

--- chp1/report.php
+++ chp1/report.php
@@ -15,17 +15,20 @@
  * Date: 2016/4/7
  * Time: 20:15
  */
-
+    $name = $_POST['firstname'] .' ' .$_POST['lastname'];
     $when_it_happened = $_POST['whenithappened'];
     $how_long = $_POST['howlong'];
     $fang_spootted = $_POST['fangspotted'];
     $email = $_POST['email'];
+    $other = $_POST['other'];
 
 echo 'Thanks for submitting the form.<br>';
+echo 'Your name is '.$name .'<br>';
 echo 'you were abducted ' .$when_it_happened .'<br>';
 echo ' and were gone for ' .$how_long .'<br>';
 echo 'was Fang there?' .$fang_spootted .'<br>';
 echo 'Your email address is ' .$email .'<br>';
+echo 'Other information is ' .$other .'<br>';
 ?>
 </body>
 </html>
--- environment/httpd-2.4.20-x86-vc14/Apache24/www/report.php
+++ environment/httpd-2.4.20-x86-vc14/Apache24/www/report.php
@@ -15,13 +15,15 @@
  * Date: 2016/4/7
  * Time: 20:15
  */
-
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
     $when_it_happened = $_POST['whenithappened'];
     $how_long = $_POST['howlong'];
     $fang_spootted = $_POST['fangspotted'];
     $email = $_POST['email'];
 
 echo 'Thanks for submitting the form.<br>';
+echo 'Your name is '.$firstname .' ' .$lastname .'<br>';
 echo 'you were abducted ' .$when_it_happened .'<br>';
 echo ' and were gone for ' .$how_long .'<br>';
 echo 'was Fang there?' .$fang_spootted .'<br>';
