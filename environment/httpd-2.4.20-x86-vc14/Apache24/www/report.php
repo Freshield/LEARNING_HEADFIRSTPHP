6bcf1d7c3bd99d5f42695d5032aaecb14e7aed98
Freshield <zxdsw199182@gmail.com>
2016-04-07 20:35:15 -0400

    first php test

---
+++ environment/httpd-2.4.20-x86-vc14/Apache24/www/report.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+    <title>Aliens Abducted Me - Report an Abduction</title>
+</head>
+
+<body>
+
+    <h2>Aliens Abducted Me - Report an Abduction</h2>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FRESHIELD
+ * Date: 2016/4/7
+ * Time: 20:15
+ */
+
+    $when_it_happened = $_POST['whenithappened'];
+    $how_long = $_POST['howlong'];
+    $fang_spootted = $_POST['fangspotted'];
+    $email = $_POST['email'];
+
+echo 'Thanks for submitting the form.<br>';
+echo 'you were abducted ' .$when_it_happened .'<br>';
+echo ' and were gone for ' .$how_long .'<br>';
+echo 'was Fang there?' .$fang_spootted .'<br>';
+echo 'Your email address is ' .$email .'<br>';
+?>
+</body>
+</html>
