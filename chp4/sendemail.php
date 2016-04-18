e93e279d872086263422ea6c5ebb8e33053f5456
Freshield <zxdsw199182@gmail.com>
2016-04-18 16:12:34 -0400

    use the loop quote

--- chp4/sendemail.php
+++ chp4/sendemail.php
@@ -1,3 +1,18 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Make Me Elvis - Send Email</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+<img src="blankface.jpg" width="161" height="350" alt="" style="float:right" />
+<img name="elvislogo" src="elvislogo.gif" width="229" height="32" border="0" alt="Make Me Elvis" />
+<p><strong>Private:</strong> For Elmer's use ONLY<br />
+    Write and send an email to mailing list members.</p>
+
+
+
 <?php
 /**
  * Created by PhpStorm.
@@ -6,50 +21,66 @@
  * Time: 19:21
  */
 
-$from = "zxdsw199182@gmail.com";
-$subject = $_POST['subject'];
-$text = $_POST['elvismail'];
+if(isset($_POST['submit'])){
 
-if(!empty($subject) && !empty($text)){
+    $from = "zxdsw199182@gmail.com";
+    $subject = $_POST['subject'];
+    $text = $_POST['elvismail'];
+    $output = false;
 
-    $query = "select * from email_list";
-    $dbc = mysqli_connect('localhost','root','8888','elvis_store') or die("Error connecting to MySQL");
-    $result = mysqli_query($dbc,$query) or die("Error querying database");
+    if(!empty($subject) && !empty($text)){
 
-    while ($row = mysqli_fetch_array($result)){
-        $firstname = $row['first_name'];
-        $lastname = $row['last_name'];
+        $query = "select * from email_list";
+        $dbc = mysqli_connect('localhost','root','8888','elvis_store') or die("Error connecting to MySQL");
+        $result = mysqli_query($dbc,$query) or die("Error querying database");
 
-        $msg = "Dear $firstname $lastname ,\n$text";
-        $to = $row['email'];
+        while ($row = mysqli_fetch_array($result)){
+            $firstname = $row['first_name'];
+            $lastname = $row['last_name'];
 
-        mail($to,$subject,$msg);
-        echo "Email sent to: $to<br>";
+            $msg = "Dear $firstname $lastname ,\n$text";
+            $to = $row['email'];
+
+            mail($to,$subject,$msg);
+            echo "Email sent to: $to<br>";
+        }
+
+        echo "<br>Send mail finished";
+        mysqli_close($dbc);
+    }
+    else{
+
+        if(empty($subject)){
+
+            echo "You forgot the email subject.<br>";
+        }
+        if(empty($text)) {
+
+            echo "You forgot the email body text.<br>";
+        }
+
+        $output = true;
+
     }
 
-    echo "<br>Send mail finished";
-    mysqli_close($dbc);
 }
 else{
-    if(empty($subject)){
-
-        echo "You forgot the email subject.<br>";
-    }
-    if(empty($text)) {
-
-        echo "You forgot the email body text.<br>";
-    }
-    ?>
-    <br>
-    <form method="post" action="sendemail.php">
-        <label for="subject">Subject of email:</label><br />
-        <input id="subject" name="subject" type="text" size="30" value="<?php echo "$subject"; ?>"/><br />
-        <label for="elvismail">Body of email:</label><br />
-        <textarea id="elvismail" name="elvismail" rows="8" cols="40"><?php echo"$text"; ?></textarea><br />
-        <input type="submit" name="Submit" value="Submit" />
-    </form>
-<?php
+    $output = true;
 }
 
+if($output == true) {
+    ?>
 
+    <br>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+        <label for="subject">Subject of email:</label><br/>
+        <input id="subject" name="subject" type="text" size="30" value="<?php echo "$subject"; ?>"/><br/>
+        <label for="elvismail">Body of email:</label><br/>
+        <textarea id="elvismail" name="elvismail" rows="8" cols="40"><?php echo "$text"; ?></textarea><br/>
+        <input type="submit" name="submit" value="Submit"/>
+    </form>
+    <?php
+}
 ?>
+</body>
+</html>
