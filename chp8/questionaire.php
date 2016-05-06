2bbedadd497f312bd579395cd27d1f72fe4ba9a0
Freshield <zxdsw199182@gmail.com>
2016-05-05 20:09:16 -0400

    done the questionnaire

--- chp8/questionaire.php
+++ chp8/questionaire.php
@@ -5,8 +5,20 @@
  * Date: 2016/5/5
  * Time: 19:05
  */
+require_once ('startsession.php');
+
+$page_title = "Questionnaire";
+require_once ('header.php');
+
 require_once ('connectvars.php');
 require_once ('appvars.php');
+
+if(!isset($_SESSION['user_id'])){
+    echo"<p class='login'>Please<a href='login.php'>log in</a> to access this page.</p>";
+    exit();
+}
+
+require_once ('navmenu.php');
 
 $dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
@@ -22,7 +34,9 @@ if(mysqli_num_rows($data) == 0){
         array_push($topicIDs,$row['topic_id']);
     }
     foreach ($topicIDs as $topic_id){
-        $query = "insert into mismatch_response user_id,topic_id values ('{$_SESSION['user_id']}','$topic_id')";
+        $query = "insert into mismatch_response (user_id,topic_id) values ('{$_SESSION['user_id']}','$topic_id')";
+
+
         mysqli_query($dbc,$query);
     }
 }
@@ -30,14 +44,17 @@ if(mysqli_num_rows($data) == 0){
 if(isset($_POST['submit'])){
     foreach ($_POST as $response_id => $response){
         $query = "update mismatch_response set response = '$response' where response_id = '$response_id'";
+        //echo "$query<br>";
         mysqli_query($dbc,$query);
     }
     echo "<p>Your responses have been saved.</p>";
 }
 
-$query = "select response_id,topic_id,response from mismatch_response where user_id = '$user_id'";
+$query = "select response_id,topic_id,response from mismatch_response where user_id = '{$_SESSION['user_id']}'";
+
+//echo "$query<br>";
 $data = mysqli_query($dbc,$query);
-$response = array();
+$responses = array();
 
 while($row = mysqli_fetch_array($data)){
     $query2 = "select name,category from mismatch_topic where topic_id = '{$row['topic_id']}'";
@@ -46,8 +63,35 @@ while($row = mysqli_fetch_array($data)){
         $row2 = mysqli_fetch_array($data2);
         $row['topic_name'] = $row2['name'];
         $row['category_name'] = $row2['category'];
-        array_push($response,$row);
+        array_push($responses,$row);
+    }else{
+        echo"some problem";
     }
 }
+
+mysqli_close($dbc);
+
+echo '<form method="post" action="' . $_SERVER['PHP_SELF'] . '">';
+echo '<p>How do you feel about each topic?</p>';
+$category = $responses[0]['category_name'];
+echo '<fieldset><legend>' . $responses[0]['category_name'] . '</legend>';
+foreach ($responses as $response) {
+    // Only start a new fieldset if the category has changed
+    if ($category != $response['category_name']) {
+        $category = $response['category_name'];
+        echo '</fieldset><fieldset><legend>' . $response['category_name'] . '</legend>';
+    }
+
+    // Display the topic form field
+    echo '<label ' . ($response['response'] == NULL ? 'class="error"' : '') . ' for="' . $response['response_id'] . '">' . $response['topic_name'] . ':</label>';
+    echo '<input type="radio" id="' . $response['response_id'] . '" name="' . $response['response_id'] . '" value="1" ' . ($response['response'] == 1 ? 'checked="checked"' : '') . ' />Love ';
+    echo '<input type="radio" id="' . $response['response_id'] . '" name="' . $response['response_id'] . '" value="2" ' . ($response['response'] == 2 ? 'checked="checked"' : '') . ' />Hate<br />';
+}
+echo '</fieldset>';
+echo '<input type="submit" value="Save Questionnaire" name="submit" />';
+echo '</form>';
+
+// Insert the page footer
+require_once('footer.php');
 
 ?>
