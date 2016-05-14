95562d198dd0ac8e01b9183ce89ec0c707862334
Freshield <zxdsw199182@gmail.com>
2016-05-14 14:45:37 -0400

    test mysql error

---
+++ last10topics/mysql_error.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FRESHIELD
+ * Date: 2016/5/14
+ * Time: 14:28
+ */
+echo "here";
+
+$dbc = mysqli_connect('localhost','root','8888','gdb');
+echo mysqli_error($dbc);
+?>
